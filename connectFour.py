6330ee477bda13b32f236c9a24afc380ef6c1dc7
Jasonsd19 <72050243+Jasonsd19@users.noreply.github.com>
2021-07-21 22:31:48 +0000

    Initial commit

---
+++ connectFour.py
@@ -0,0 +1,233 @@
+boardRef = [
+         0 , 1 , 2 , 3 , 4 , 5  , 6,
+         7 , 8 , 9 , 10, 11, 12, 13,
+         14, 15, 16, 17, 18, 19, 20,
+         21, 22, 23, 24, 25 ,26, 27,
+         28, 29 ,30, 31, 32, 33, 34,
+         35, 36, 37, 38, 39, 40, 41
+         ]
+col1 = [0, 7 , 14 ,21 ,28 ,35]
+col2 = [1, 8 , 15, 22, 29, 36]
+col3 = [2, 9 , 16, 23, 30, 37]
+col4 = [3, 10, 17, 24, 31, 38]
+col5 = [4, 11, 18, 25, 32, 39]
+col6 = [5, 12, 19 ,26, 33, 40]
+col7 = [6, 13, 20, 27, 34, 41]
+
+class Board:
+  # Don't ask me why I made classes, I don't even know why I did
+    class Node:
+        def __init__(self, colour=None):
+            # colour can be either 'r', 'y', or None
+            self.colour = colour
+
+        def getColour(self):
+            return self.colour
+
+        def setColour(self, colour):
+            if colour == 'r':
+                self.colour = ":red_circle:"
+            elif colour == 'y':
+                self.colour = ":yellow_circle:"
+            else:
+                return
+
+    def __init__(self):
+        self.board = [self.Node() for i in range(42)]
+
+    def makeMove(self, player, col):
+        try:
+            moveIndex = self.place_piece(player, col)
+            if self.hasWon(moveIndex):
+                return True
+            else:
+                return False
+        except:
+            raise Exception()
+
+    def hasWon(self, moveIndex):
+        c = self.checkColumn(moveIndex)
+        r = self.checkRow(moveIndex)
+        d = self.checkDiagonals(moveIndex)
+        if c or r or d:
+            return True
+        else:
+            return False
+
+    def checkColumn(self, index):
+        colour = self.board[index].getColour()
+        upIndex = index + 7
+        streak = 1
+        while upIndex >= 0 and upIndex < 42:
+            if self.board[upIndex].getColour() == colour:
+                streak += 1
+                if streak >= 4:
+                    return True
+            else:
+                break
+            upIndex += 7
+        downIndex = index - 7
+        streak = 1
+        while downIndex >= 0 and downIndex < 42:
+            if self.board[downIndex].getColour() == colour:
+                streak += 1
+                if streak >= 4:
+                    return True
+            else:
+                break
+            downIndex -= 7
+        return False
+
+    def checkRow(self, index):
+        colour = self.board[index].getColour()
+        row = index // 7
+        low = row * 7
+        high = low + 6
+        rIndex = index + 1
+        streak = 1
+        while rIndex >= low and rIndex <= high:
+            if self.board[rIndex].getColour() == colour:
+                streak += 1
+                if streak >= 4:
+                    return True
+            else:
+                break
+            rIndex += 1
+        lIndex = index - 1
+        streak = 1
+        while lIndex >= low and lIndex <= high:
+            if self.board[lIndex].getColour() == colour:
+                streak += 1
+                if streak >= 4:
+                    return True
+            else:
+                break
+            lIndex -= 1
+        lIndex = index + -1
+        rIndex = index + 1
+        streak = 1
+        # Jank solution can be incorporated into earlier checks lol xD haha
+        while lIndex >= low and rIndex >= low and lIndex <= high and rIndex <= high:
+            if self.board[rIndex].getColour() == colour:
+                streak +=1
+                if streak >= 4:
+                    return True
+            if self.board[lIndex].getColour() == colour:
+                streak += 1
+                if streak >= 4:
+                    return True
+            else:
+                break
+            lIndex -= 1
+            rIndex += 1
+        return False
+
+    def checkDiagonals(self, index):
+        colour = self.board[index].getColour()
+        ulIndex = index - 8
+        streak = 1
+        while ulIndex >= 0 and ulIndex < 42:
+            if self.board[ulIndex].getColour() == colour:
+                streak += 1
+                if streak >= 4:
+                    return True
+            else:
+                break
+            ulIndex -= 8
+        urIndex = index - 6
+        streak = 1
+        while urIndex >= 0 and urIndex < 42:
+            if self.board[urIndex].getColour() == colour:
+                streak += 1
+                if streak >= 4:
+                    return True
+            else:
+                break
+            urIndex -= 6
+        brIndex = index + 8
+        streak = 1
+        while brIndex >= 0 and brIndex < 42:
+            if self.board[brIndex].getColour() == colour:
+                streak += 1
+                if streak >= 4:
+                    return True
+            else:
+                break
+            brIndex += 8
+        blIndex = index + 6
+        streak = 1
+        while blIndex >= 0 and blIndex < 42:
+            if self.board[blIndex].getColour() == colour:
+                streak += 1
+                if streak >= 4:
+                    return True
+            else:
+                break
+            blIndex += 6
+        return False
+
+    def place_piece(self, player, col):
+        # player is an integer, either 1(Yellow) or 2(Red)
+        # col is the column the player wants to drop the piece
+        # Future Jason here - IDK why I did this
+        if player == 1 and self.isValid(col):
+            index = self.place('y', col)
+            return index
+        elif player == 2 and self.isValid(col):
+            index = self.place('r', col)
+            return index
+        else:
+            raise Exception()
+
+    def isValid(self, col):
+        return self.board[col - 1].getColour() == None
+
+    def place(self, colour, col):
+        column = self.getColumn(col)
+        for i in range(-1, -7, -1):
+            index = column[i]
+            if self.board[index].getColour() == None:
+                self.board[index].setColour(colour)
+                return index
+
+    def getColumn(self, col):
+        if col == 1:
+            return col1
+        elif col == 2:
+            return col2
+        elif col == 3:
+            return col3
+        elif col == 4:
+            return col4
+        elif col == 5:
+            return col5
+        elif col == 6:
+            return col6
+        else:
+            return col7
+
+    def print_board(self):
+      # Pain in the ass to create
+        result = ''
+        i = 0
+        for node in self.board:
+            if node.getColour() == None:
+                nodeString = ':white_circle:'
+            else:
+                nodeString = node.getColour()
+            if i%7 == 0 and i != 0:
+                result += '\n═════════════════════════\n'
+                result += '│' + nodeString + '│'
+            else:
+                result += '│' + nodeString + '│'
+            i += 1
+        return result
+
+def play(b, player, col):
+  # I literally don't even use this, why is this here?
+    try:
+        isOver = b.makeMove(player, col)
+        boardState = b.print_board()
+    except:
+        return -1
+    return (b, boardState, isOver)
---
+++ main.py
@@ -0,0 +1,408 @@
+import os
+import discord
+import requests
+import ffmpeg
+import datetime
+import random
+import asyncio
+from discord.ext import tasks, commands
+from keep_alive import keep_alive
+from replit import db
+import connectFour as c4
+
+bot = commands.Bot(command_prefix='$')
+
+game = discord.Game("$help for more information")
+
+sad_words  = {"sad", "horrible", "lonely", "depressed", ":(", "gloomy", "tragic", "despair", "pain", "forlorn", "hurt", "hurting"}
+
+sleep_words = {"sleep", "sleeping", "going to bed"}
+
+isActive = False
+isActive2 = False
+
+#Audio Vars
+voiceChannel = None
+isPlayingAudio = False
+leaveTimer = None
+#Audio Vars
+
+#Game Vars
+isPlaying = False
+player1 = None
+player2 = None
+gameMessage = None
+turn = None
+board = None
+#Game Vars
+
+winnersID = 817519861906669640
+
+@bot.event
+async def on_ready():
+  print("We have logged in as {0.user}".format(bot))
+  await bot.change_presence(status=discord.Status.online, activity=game)
+
+@bot.event
+async def on_message(message):
+  global isActive
+  global isActive2
+
+  if message.author == bot.user or message.author.bot:
+    return
+
+  if message.author != bot.user:
+    isActive = True
+    isActive2 = True
+
+  if sad_words.intersection(message.content.lower().split()):
+    await message.channel.send("Well {0}, This server has friends that still care and love you <:patW:673433778944737310>.".format(message.author.mention))
+    return
+
+  if sleep_words.intersection(message.content.lower().split()):
+    await message.channel.send("Good night {0}, hope you enjoyed your stay <:peepolove:822337341892853790>.".format(message.author.mention))
+    return
+
+  if winnersID in message.raw_role_mentions:
+    await message.add_reaction("<:pogNuts:827445043018858526>")
+    return
+
+  if bot.user.id in message.raw_mentions:
+    if message.author.id == 387855771355840512:
+      await message.channel.send("I am at your service oh great and powerful Jason <:o7:822333430166454312>.")
+      return
+    await message.channel.send("<:wutface:823056657110007839> Don't @ me loser")
+    return
+
+  await bot.process_commands(message)
+
+@bot.command(description='See what is trending on WSB.',
+             help='Displays the 10 most popular tickers from the WallStreetBets subreddit.')
+async def wsb(ctx):
+  await ctx.send(topTickers())
+
+@bot.command(description='use format\n$suggest [message]\nTo register your suggestion. Jason can review them later',
+help='Simply enter any suggestion and it will be recorded in our database.')
+async def suggest(ctx, *, message: str):
+  try:
+    db["Suggestions"].append(message)
+    await ctx.send("Thanks for your suggestion, it has been recorded!")
+  except:
+    await ctx.send("Sorry there was an error recording your suggestion :(")
+
+@bot.command(description='Lists all the suggestions',
+             help='Only Jason can use this :)')
+async def list(ctx):
+  if ctx.author.id == 387855771355840512:
+    result = 'Suggestions:\n'
+    i = 1
+    for suggestion in db['Suggestions']:
+      result += str(i) + ': ' + suggestion + '.\n'
+      i += 1
+    await ctx.send(result)
+  else:
+    await ctx.send("Nice try, but you're not Jason.")
+
+@bot.command(description='Delete suggestion at given index\n$remove 0\nWill remove the first suggestion.',
+help='Only Jason can use this :)')
+async def delete(ctx, index: int):
+  if ctx.author.id == 387855771355840512:
+    if len(db['Suggestions']) == 0:
+      await ctx.send("There is nothing to delete!")
+      return
+    try:
+      removed = db['Suggestions'].pop(index)
+      await ctx.send("Successfully removed suggestion:\n" + removed)
+    except:
+      await ctx.send("Unable to remove given index, perhaps it doesn't exist?")
+  else:
+    await ctx.send("Nice try, but you're not Jason.")
+
+@bot.command()
+async def catFact(ctx):
+  await ctx.send(getCatFact())
+
+@bot.command()
+async def animalPic(ctx):
+  await ctx.send(getAnimalPic())
+
+@bot.command()
+async def meme(ctx):
+  await ctx.send(getMeme())
+
+@bot.command()
+async def insultMe(ctx):
+  user = ctx.author.mention
+  if ctx.author.id == 387855771355840512:
+    await ctx.send("I would never insult my creator.")
+  else:
+    await ctx.send(user + getInsult())
+
+@bot.command()
+async def insult(ctx, mention: str):
+  try:
+    if mention[0] == '<' and mention[-1] == '>':
+      if mention == '<@!387855771355840512>':
+        await ctx.send("I would never insult my creator, but I would insult you\n" + ctx.author.mention + getInsult())
+      else:
+        await ctx.send(mention + ' ' + getInsult())
+    else:
+      raise Exception("Incorrect format")
+  except:
+    await ctx.send("Incorrect format")
+
+@bot.command()
+async def roll(ctx, dice):
+  if dice == "d6":
+    r = requests.get('http://roll.diceapi.com/html/d6')
+    # Holy jank batman
+    text = r.text
+    link = text.split("\"")[1]
+    await ctx.send(link)
+    # Can't believe this worked lol
+  elif dice == "d20":
+    r = requests.get('http://roll.diceapi.com/html/d20')
+    # I'll fkin do it again
+    text = r.text
+    link = text.split("\"")[1]
+    await ctx.send(link)
+    # Told you
+  else:
+    await ctx.send("Invalid format!")
+
+@bot.command()
+async def join(ctx):
+  global voiceChannel
+  global leaveTimer
+  if voiceChannel == None:
+    connected = ctx.author.voice
+    if connected:
+      voiceChannel = await connected.channel.connect()
+      await asyncio.sleep(1)
+      leaveTimer = datetime.datetime.now() + datetime.timedelta(hours=1)
+      await asyncio.sleep(3600)
+      if datetime.datetime.now() >= leaveTimer and voiceChannel != None:
+        await voiceChannel.disconnect()
+        await ctx.send("I have left the voice channel due to inactivity.")
+        voiceChannel = None
+    else:
+      await ctx.send("You need to be in a voice channel!")
+
+@bot.command(description='$play [filename]\nFilename has to be a file in a local directory\nThe bot will join the voice channel the user is in, play the audio file, then leave.',
+help='Allows the bot to join in the voice chat and play an audio file!')
+async def play(ctx, name):
+  global voiceChannel
+  global isPlayingAudio
+  global leaveTimer
+  if voiceChannel != None and isPlayingAudio == False:
+    isPlayingAudio = True
+    try:
+      voiceChannel.play(discord.FFmpegPCMAudio(name + '.mp3'))
+      while voiceChannel.is_playing():
+        await asyncio.sleep(0.1)
+      await asyncio.sleep(1)
+      voiceChannel.stop()
+    except:
+      await ctx.send("Unable to play music file, either you entered an incorrect name or it does not exist.")
+    finally:
+      isPlayingAudio = False
+      leaveTimer = datetime.datetime.now() + datetime.timedelta(hours=1)
+      await asyncio.sleep(3600)
+      if datetime.datetime.now() >= leaveTimer and voiceChannel != None:
+        await voiceChannel.disconnect()
+        await ctx.send("I have left the voice channel due to inactivity.")
+        voiceChannel = None
+
+@bot.command()
+async def leave(ctx):
+  global voiceChannel
+  if voiceChannel != None:
+    await voiceChannel.disconnect()
+    voiceChannel = None
+
+@tasks.loop(hours=2)
+async def reminder():
+  global isActive
+  if isActive:
+    channel = bot.get_channel(866903190082682910)
+    await channel.send("Remember to stand up and exercise (or stretch)! Also drink some water and stay hydrated! Especially you <@187083047340998656> ")
+    isActive = False
+
+@reminder.before_loop
+async def before_reminder():
+  await bot.wait_until_ready()
+
+@tasks.loop(hours=5)
+async def remindTest():
+  global isActive2
+  if isActive2:
+    channel = bot.get_channel(866903190082682910)
+    await channel.send("<@!628081471785009162> Study for your test or you're gonna fail - Patrick")
+    isActive2 = False
+
+@remindTest.before_loop
+async def before_remindTest():
+  await bot.wait_until_ready()
+
+@tasks.loop(hours=16)
+async def factAndPicAndMeme():
+  animChannel = bot.get_channel(802692284558475305)
+  memeChannel = bot.get_channel(866938492596387850)
+  await animChannel.send(getCatFact())
+  await animChannel.send("Now enjoy a picture of a random animal.")
+  await asyncio.sleep(2)
+  await animChannel.send(getAnimalPic())
+  await asyncio.sleep(2)
+  await memeChannel.send(getMeme())
+
+@factAndPicAndMeme.before_loop
+async def before_factAndPic():
+  await bot.wait_until_ready()
+
+def topTickers():
+  r = requests.get("https://apewisdom.io/api/v1.0/filter/wallstreetbets")
+  json = r.json()
+  result = "Top 10 tickers in WSB:\n"
+  for i in range(10):
+    result += "#{0} {1}\n".format(i+1, json['results'][i]['ticker'])
+  return result
+
+def getCatFact():
+  r = requests.get('https://catfact.ninja/fact?max_length=500')
+  json = r.json()
+  return json['fact']
+
+def getAnimalPic():
+  i = random.randint(1, 99)
+  if i > 66:
+    r = requests.get('https://aws.random.cat/meow?ref=apilist.fun')
+    return r.json()['file']
+  elif i > 33:
+    r = requests.get('https://random.dog/woof.json?ref=apilist.fun')
+    return r.json()['url']
+  else:
+    r = requests.get('https://randomfox.ca/floof/')
+    return r.json()['image']
+
+def getMeme():
+  r = requests.get('https://meme-api.herokuapp.com/gimme')
+  return r.json()['url']
+
+def getInsult():
+  r = requests.get('https://evilinsult.com/generate_insult.php?lang=en&type=text')
+  string = r.text
+  return string
+
+###################################
+#           Connect Four          #
+###################################
+
+@bot.command()
+async def connect4(ctx, mention1: str, mention2: str):
+  global isPlaying
+  global player1
+  global player2
+  global gameMessage
+  global turn
+  global board
+  if (mention1[0] == '<' and mention1[-1] == '>' and mention2[0] == '<' and mention2    [-1] == '>' and isPlaying == False):
+    board = c4.Board()
+    turn = 1
+    embed = createEmbed(board.print_board())
+    gameMessage = await ctx.send(embed=embed)
+    reactions = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣']
+    for reaction in reactions:
+      await gameMessage.add_reaction(reaction)
+    isPlaying = True
+    player1 = (mention1, mention1.replace('!', ''))
+    player2 = (mention2, mention2.replace('!', ''))
+  else:
+    await ctx.send("Invalid format")
+
+@bot.command()
+async def killGame(ctx):
+  cleanupGame()
+  await ctx.send("Game has been stopped.")
+
+@bot.event
+async def on_reaction_add(reaction, user):
+  if user.bot:
+    return
+  ############# Everything below is ugly, but it works ;) ############
+  global isPlaying
+  global gameMessage
+  global player1
+  global player2
+  global gameMessage
+  global turn
+  global board
+  if isPlaying == True:
+    try:
+      if reaction.message == gameMessage and (user.mention == player1[0] or user.mention == player1[1]) and turn == 1:
+        col = getColumn(reaction.emoji)
+        isOver = board.makeMove(1, col)
+        winner = player1[0]
+        if turn == 1:
+          turn = 2
+        else:
+          turn = 1
+        embed = createEmbed(board.print_board())
+      elif reaction.message == gameMessage and (user.mention == player2[0] or user.mention == player2[1]) and turn == 2:
+        col = getColumn(reaction.emoji)
+        isOver = board.makeMove(2, col)
+        winner = player2[0]
+        if turn == 1:
+          turn = 2
+        else:
+          turn = 1
+        embed = createEmbed(board.print_board())
+      else:
+        return
+      await gameMessage.edit(embed=embed)
+      if isOver:
+        await reaction.message.channel.send(winner + " Congratulations you win!")
+        cleanupGame()
+    except:
+      await reaction.message.channel.send("Invalid move, try again!")
+    finally:
+      await reaction.remove(user)
+    return
+
+def createEmbed(content):
+  global turn
+  embed = discord.Embed()
+  embed.add_field(name='Connect Four - Player ' + str(turn) + ' it is your turn', value=content ,inline=False)
+  return embed
+
+def getColumn(emoji):
+  i = 1
+  reactions = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣']
+  for reaction in reactions:
+    if emoji == reaction:
+      return i
+    else:
+      i += 1
+
+def cleanupGame():
+  global isPlaying
+  global player1
+  global player2
+  global gameMessage
+  global turn
+  global board
+  isPlaying = False
+  player1 = None
+  player2 = None
+  gameMessage = None
+  turn = None
+  board = None
+
+##########################################
+#           Rock Paper Scissors          # (do later lol)
+##########################################
+
+reminder.start()
+remindTest.start()
+factAndPicAndMeme.start()
+keep_alive()
+bot.run(os.environ['token'])
